41d9078e8d3988e6f99d432a1239f5c626f7fbce
KubzThe14th <kubiak014@gmail.com>
2024-08-23 11:00:10 +0800

    base commit

---
+++ src/main/java/com/assignement/realtimequoteviewer/RealTimeQuoteViewerApplication.java
@@ -0,0 +1,13 @@
+package com.assignement.realtimequoteviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeQuoteViewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeQuoteViewerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assignement/realtimequoteviewer/RealTimeQuoteViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignement.realtimequoteviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeQuoteViewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
