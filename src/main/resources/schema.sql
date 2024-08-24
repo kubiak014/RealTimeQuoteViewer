a5a51276d23db279659419950562aac398eadf17
KubzThe14th <kubiak014@gmail.com>
2024-08-24 14:03:04 +0800

    added new seceurities in DB build script

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,6 @@
 create table SECURITY(
                       TICKER_ID varchar(50) not null,
                       SECURITY_TYPE varchar(30) not null,
+
                       PRIMARY KEY ( TICKER_ID )
 );
